829978ca200ded64b6706e256e62103f2b874a5c
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2026-04-09 19:49:37 +0300

    fix: standardize $0 handling typo

--- gitignore.plugin.zsh
+++ gitignore.plugin.zsh
@@ -1,4 +1,4 @@
-# Standarized $0 handling, following:
+# Standardized $0 handling, following:
 # https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 export ZSH_PLUGIN_GITIGNORE_PATH="${0:h}"
